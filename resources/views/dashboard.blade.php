c8c5bf834863f62b95126a534c5ddc9a762d35e9
luckyramdani <lucky.ramdani.tif16@polban.ac.id>
2018-08-16 11:37:55 +0700

    Revert "test"
    This reverts commit ec64a0f901de627ef041c0c99814905a7ee4617a.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,7 +20,7 @@
 
 @section('title')
 
-<!-- Dashboard-->  <h1 style="margin:20px; font-size:20px; color:#EDEDED;">DASHBOARD</h1>
+  <h1 style="margin:20px; font-size:20px; color:#EDEDED;">DASHBOARD</h1>
 
 @endsection
 
