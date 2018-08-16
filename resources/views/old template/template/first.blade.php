fb3570cd0319c8ac08812121246e9fbfdbc0fd28
AgitPrasetya <agitprasetya00@gmail.com>
2018-08-16 15:12:11 +0700

    Delete old template

--- resources/views/old template/template/first.blade.php
+++
@@ -1,5 +0,0 @@
-    <div class="container body">
-      <div class="main_container">
-        <div class="col-md-3 left_col menu_fixed">
-          <div class="left_col scroll-view">
-            <div class="navbar nav_title" style="border: 0;">
